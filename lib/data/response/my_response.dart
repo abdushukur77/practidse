017e405249abec856d06df19234e069dad0542a9
Yorq1nnur <abduraimovyorqinjon4@gmail.com>
2024-04-10 02:02:34 +0500

    1

--- lib/data/response/my_response.dart
+++
@@ -1,9 +0,0 @@
-class MyResponse {
-  dynamic data;
-  String errorText;
-
-  MyResponse({
-    this.errorText = '',
-    this.data,
-  });
-}
--- lib/utils/app_constants/app_constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String apiKey =
-      "dDtCUgqdEttc1TV9-_UAQ36XSlV-MgsDLjAKrk9vuRKW075fRg";
-  static const String baseUrl = "https://crudapi.co.uk";
-  static const String endPoint = "/api/v1/cards";
-  static RegExp cardNumberRegExp = RegExp(r'^\d{16}$');
-}
